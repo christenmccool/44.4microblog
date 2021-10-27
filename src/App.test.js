dc008ab5f7224531afc33c7600c7cf0505185bb0
Christen McCool <christenmccool@gmail.com>
2021-10-27 13:08:09 -0700

    Titlelist test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,33 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import {MemoryRouter} from 'react-router-dom';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import thunk from "redux-thunk";
+import rootReducer from './rootReducer';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+test('renders without crashing', () => {
+  render( <Provider store={store}>
+           <MemoryRouter>
+             <App />
+            </MemoryRouter>
+          </Provider>);
 });
+
+test('matches snapshot', () => {
+  const {useFragment} = render( <Provider store={store}>
+                                  <MemoryRouter>
+                                    <App />
+                                    </MemoryRouter>
+                                  </Provider>
+                                );
+  expect(useFragment).toMatchSnapshot();
+});
+
+
+
+
+
